a0bcb3d910cd7dffa2d666f550c3128b229ee5f8
byggemandboesen <victorboesen@hotmail.com>
2021-10-20 13:35:35 +0200

    :sparkles: Added a couple technical analysis functions

--- analysis.py
+++ analysis.py
@@ -1,8 +1,32 @@
 import numpy as np
 from ta import momentum, trend
 
-class Analyser:
+class Analyzer:
 
-    def __init__(self, df):
-        self.closes = df["Close"]
+    def __init__(self, parameters):
+        self.TakeProfit = parameters["TakeProfit"]
+        self.MaxLoss = parameters["MaxLoss"]
+        self.StopLoss = parameters["StopLoss"]
+        self.Oversolf = parameters["Oversold"]
+        self.Overbought = parameters["Overbought"]
+    
+    def ema(self, closes, windows):
+        ema = trend.EMAIndicator(closes, windows)
+        return ema.ema_indicator()
+
+    def rsi(self, closes, windows = 14):
+        rsi = momentum.RSIIndicator(closes, windows)
+        return rsi.rsi()
+
+    def doAnalysis(self, df):
+        # Get closes as floats
+        closes = df["Close"].astype("float")
+        
+        # Calculate indicators
+        df["EMA25"] = self.ema(closes, 25)
+        df["EMA50"] = self.ema(closes, 50)
+        df["RSI"] = self.rsi(closes)
+
+        # Finally, drop NaN rows
+        df.dropna()
         
